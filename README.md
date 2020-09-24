b6cb19290ed42609ff6a0a8c74998fffda89696e
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2020-09-24 10:53:23 +0400

    update README and added exercises > debugging html + JS

--- README.md
+++ README.md
@@ -2,57 +2,79 @@
 
 ## Notes
 
-- Chrome Dev Tools
-  - Shortcut for Console `Ctrl` `Shift` `J`
-  - Shortcut for Inspect Element `Ctrl` `Shift` `I`
-- Windows Command Prompt vs. Linux Terminal
-  - Clear: `cls` vs `clear`
-  - List directories: `dir` vs `ls`
-- Relative file paths
-  - same directory: `./file.js`
-  - one directory up: `../file.js`
-- Running and loading JS
-  - In command prompt/ terminal via node: `node file.js`
-- Tooling
-  - ESLint
-    - JS Linter tool
-    - to indentify/ report on JS issues (bad practices/ design patterns, unused variables/ functions)
-    - modifiable config - can use someone else's like from [wes bos](https://github.com/wesbos/eslint-config-wesbos)
-    - best practice to set the cofig locally, but can do globally
-    - pluggable - many plugins available e.g. for Vue, React, Angular, Prettier
-  - Prettier
-    - Formats code
-    - quit opinionated, only a few settings availabel to toggle
+### Chrome Dev Tools
+
+- Shortcut for Console `Ctrl` `Shift` `J`
+- Shortcut for Inspect Element `Ctrl` `Shift` `I`
+
+### Windows Command Prompt vs. Linux Terminal
+
+- Clear: `cls` vs `clear`
+- List directories: `dir` vs `ls`
+
+### Relative file paths
+
+- same directory: `./file.js`
+- one directory up: `../file.js`
+
+### Running and loading JS
+
+- In command prompt/ terminal via node: `node file.js`
+
+### Tooling
+
+- ESLint
+  - JS Linter tool
+  - to indentify/ report on JS issues (bad practices/ design patterns, unused variables/ functions)
+  - modifiable config - can use someone else's like from [wes bos](https://github.com/wesbos/eslint-config-wesbos)
+  - best practice to set the cofig locally, but can do globally
+  - pluggable - many plugins available e.g. for Vue, React, Angular, Prettier
+- Prettier
+  - Formats code
+  - quit opinionated, only a few settings availabel to toggle
 - Node
   - `npm init` via terminal for your project folder, in order to create a `package.json` file for depdendencies like ESLint and Prettier
-- Types in JS
 
-  - SNOB'N'US
-  - String
-  - Number
-    - In console: window.location = \`https://\${0.1+0.2}.com\`;
-    - https://0.30000000000000004.com/
-    - better to work with whole numbers i.e. cents rather than dollars
-  - Object - everything in JS is an object, so this is a special case
-  - Boolean
-  - Null
-  - Undefined
-  - Symbol - unique identifier
+### Types in JS
 
-- Functions
-  - allow us to group together sets of statements
-  - functions take in data as arguements and can return a value
-  - functions vs parameters
-  - function vs method
-  - Built-in functions
-    - `Math.max(10,12)`
-    - `parseFloat('20.234234')`
-    - `parseInt('20.234234')`
-    - `Date.now()` returns number of milliseconds in epoch time (since 1st Jan 1970)
-    - `document.querySelector('p')`
-    - `navigator.vibrate(200)`, mobile functions
-    - `scrollTo(0,600)`
-    - `scrollTo({top: 500, left: 0, behaviour: 'smooth'})`
+- SNOB'N'US
+- String
+- Number
+  - In console: window.location = \`https://\${0.1+0.2}.com\`;
+  - https://0.30000000000000004.com/
+  - better to work with whole numbers i.e. cents rather than dollars
+- Object - everything in JS is an object, so this is a special case
+- Boolean
+- Null
+- Undefined
+- Symbol - unique identifier
+
+### Functions
+
+- allow us to group together sets of statements
+- functions take in data as arguements and can return a value
+- functions vs parameters
+- function vs method
+- Built-in functions examples
+  - `Math.max(10,12)`
+  - `parseFloat('20.234234')`
+  - `parseInt('20.234234')`
+  - `Date.now()` returns number of milliseconds in epoch time (since 1st Jan 1970)
+  - `document.querySelector('p')`
+  - `navigator.vibrate(200)`, mobile functions
+  - `scrollTo(0,600)`
+  - `scrollTo({top: 500, left: 0, behaviour: 'smooth'})`
 - DRY - don't repeat your self, keep your code DRY
-- git
-- github
+
+### Debugging
+
+- Console Methods
+- Callstack
+- Grabbing Elements
+- Breakpoints
+- Scope
+- Network Requests
+
+### git
+
+### github
---
+++ exercises/001-debugging/debugging.js
@@ -0,0 +1,158 @@
+const people = [
+  { name: 'Wes', cool: true, country: 'Canada' },
+  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Snickers', cool: false, country: 'Dog Country' },
+];
+
+people.forEach((person, index) => {
+  console.groupCollapsed(`Los for ${person.name}`);
+  console.log(person.country);
+  console.log(person.cool);
+  console.log('DONE!');
+  console.groupEnd(`Los for ${person.name}`);
+
+  console.log(person.name);
+
+  if (person.name === 'Wes') {
+    console.error('wrong name');
+    console.warn('check name');
+    console.table(people); // good for a list of objects
+  }
+});
+
+function doALotOfStuff() {
+  console.group('Doing some stuff');
+  console.log('log');
+  console.error('error');
+  console.warn('warning');
+  console.groupEnd('Doing some stuff');
+}
+
+// debugger keyword example
+people.forEach((person, index) => {
+  // debugger;
+  console.log(`2nd forEach: ${person.name}`);
+});
+
+// Debugging methods
+/**
+ * 98% of debugging is done with console.log, breakpoints and network requests
+ * the other methods are handy in edge cases *
+ */
+
+// Console Methods
+/**
+ * console.log(), console.error(), console.warn(),
+ * console.table(), good for a list of objects
+ * console.count(), shows how many times something has run
+ * console.group() or console.groupCollapsed() with console.groupEnd(), useful for grouping all logs between into a collapsable group
+ */
+
+// Callstack or Stack Trace
+/**
+ * Tells you what function called what function
+ * This will be important when going from one file to another
+ * e.g. go() calls doctorize() and greet(), which calls doesntExist()
+ * doesntExist() will cause an error in console
+ * read the callstack in the console to see where the error was
+ * error was a greet() on line 72, which was called by go() on line 77, which was called by bootstrap() on line 83
+ * Uncaught ReferenceError: doesntExist is not defined
+    at greet (debugging.js:72)
+    at go (debugging.js:77)
+    at bootstrap (debugging.js:83)
+    at <anonymous>:1:1
+ */
+
+// Grabbing Elements
+/**
+ * e.g. go to https://developer.mozilla.org/en-US/
+ * inspect the search bar in Elements tab
+ * switch over to Console and enter $0, this will return the element currently selected in Elements tab
+ * a shorthand to access the last element clicked, this is not jQuery
+ * you can check $0.value
+ * $0 is last element clicked, $1 is second last element clicked, and so on
+ * $ and $$ are shorthand selectors for elements
+ * e.g. $('p') matches the first <p> element
+ * and $$('p') matches all <p> elements
+ */
+
+// Breakpoints
+/**
+ * debugger; keyword will pause JS from running only when console is open
+ * this adds a breakpoint
+ * the Sources tab shows the callstack, variables and their scope
+ * and you can step over each line of code from there on
+ * or click play to continue running code until you hit the next debugger
+ * This is good if console loggging is overwhelming
+ * Be sure to remove 'debugger;' from your code
+ * Also, in the Sources tab, navigate to the .js file and click on any line to add a breakpoint
+ */
+
+// Scope
+/**
+ * Scope will let us peer into what variables are
+ *
+ */
+
+// Network Requests
+/**
+ * dev tools Network tab
+ * shows all the files need to make this site work
+ * in our case debugging.html, base.css, debugging.js, data:image/svg+xml, icanhazdadjoke.com
+ * you can see info about the request: raw response (useful when looking at JSON), a preview and time taken (where the time was taken in that request)
+ * e.g. for icanhazdadjoke.com request, 600ms used in waiting for the api to return the joke
+ * filter by XHR (data being sent out), JS, CSS, Img, Media
+ */
+
+// Break On Attribute
+/**
+ * Another way to add breaks
+ * inspect element > in elements tab, right click element > break on > attribute modification (e.g. change font size)
+ * (also subtree modification, e.g when something adds a div or something)
+ * (also node removal, e.g. when somebody removes an element or text from something)
+ * Break On Attribute is not used often, but useful when exploring a random code base
+ * Sources tab, Event Listener Breakpoints can be toggled on/off
+ * e.g. Event Listener Breakpoint > Mouse > Click
+ * Sources tab, XHR breakpoint for anytime a fetch request is made to an external API, it'll break for specified API url or all requests
+ */
+
+// Some Setup Code
+
+function doctorize(name) {
+  console.count(`running doctorize for ${name}`);
+  return `Dr. ${name}`;
+}
+
+function greet(name) {
+  doesntExist(); // will cause an error
+  return `Hello ${name}`;
+}
+
+function go() {
+  const name = doctorize(greet('Wes'));
+  console.log(name);
+}
+
+function bootstrap() {
+  console.log(`Starting the app!`);
+  go();
+}
+
+const button = document.querySelector('.bigger');
+button.addEventListener('click', function (e) {
+  const newFontSize =
+    parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+  e.currentTarget.style.fontSize = `${newFontSize}px`;
+});
+
+// A Dad joke fetch
+async function fetchDadJoke() {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'text/plain',
+    },
+  });
+  const joke = await res.text();
+  console.log(joke);
+  return joke;
+}
